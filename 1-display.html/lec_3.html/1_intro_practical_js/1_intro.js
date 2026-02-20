982a9058209dfcf80d063051b74cc01ed8e3dc92
vicky kumar <computerscience027@gmail.com>
2026-02-20 12:44:32 +0530

    combinator in css

---
+++ 1-display.html/lec_3.html/1_intro_practical_js/1_intro.js
@@ -0,0 +1,26 @@
+console.log("inside intro");
+
+// varible declarition->varname is a
+//  variable that cuurently holds undefined values.
+
+let varName;
+// default value of a variable is undefined.
+console.log("5",varName);
+console.log("7",varName);
+// js->nummeric value
+varName=11;
+console.log("12",varName);
+varName=10.2;
+console.log("23",varName);
+
+
+// strings
+let newWar;
+console.log("15",newWar);
+
+
+newWar="i am string"
+console.log("18",newWar);
+// boolean
+newWar=true;
+console.log("18",newWar);
